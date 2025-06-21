aa90ed0c0575374878606c16a1997fb36be9b1c9
Nate Willis <natewillis1@gmail.com>
2025-06-21 00:34:10 +0000

    fixed init in settings

--- omaorbit/omaorbit/settings/__init__.py
+++ omaorbit/omaorbit/settings/__init__.py
@@ -1,5 +1,11 @@
 import os
 
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).resolve().parent.parent.parent / '.env')
+
 # Default to development settings
 environment = os.environ.get('DJANGO_ENV', 'dev')
 
